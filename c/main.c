b17a187888f37a6cbcc14b308420ac795cc30b51
Ali Rıza Şahin <riza.sahin@getir.com>
2022-01-16 13:16:16 +0300

    Initial Commit

---
+++ c/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  c
+//
+//  Created by Ali Rıza Şahin on 16.01.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
